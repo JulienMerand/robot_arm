1a76a9a09a0e021e8875db4b38961183e48c31d9
Julien <julien.merand@etu.imt-nord-europe.fr>
2023-04-02 22:59:06 +0200

    Config moveit2

---
+++ build/robot_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/julien/Documents/ros/robot_arm/src/robot_moveit_config
+# Build directory: /home/julien/Documents/ros/robot_arm/build/robot_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
