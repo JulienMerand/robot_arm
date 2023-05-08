6bc2468205957bc430802764eed7ab64b7fdcf64
Julien <julien.merand@etu.imt-nord-europe.fr>
2023-05-08 12:07:09 +0200

    congig Rviz2 + moveit2

---
+++ src/prog_robot/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'prog_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='julien',
+    maintainer_email='julien.merand@etu.imt-nord-europe.fr',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
--- src/robot_moveit_config/launch/demo.launch.py
+++ src/robot_moveit_config/launch/demo.launch.py
@@ -3,8 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = (MoveItConfigsBuilder("Robot", package_name="robot_moveit_config")
-                     .robot_description(file_path="config/Robot.urdf.xacro")
-                     .robot_description_semantic(file_path="config/Robot.srdf")
-                     .to_moveit_configs())
+    moveit_config = MoveItConfigsBuilder("Robot", package_name="robot_moveit_config").to_moveit_configs()
     return generate_demo_launch(moveit_config)
